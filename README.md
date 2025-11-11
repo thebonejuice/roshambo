e5ece6d6d4504cdc6487b55e948a962dd859cea3
Hugo Casas <155012694+thebonejuice@users.noreply.github.com>
2025-11-11 13:55:25 -0600

    Add getHumanChoice function and getComputerChoice function

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 hi yes that's me
+
+So, we have to create a rock, paper, scissors game (by the name of Roshambo) for the Odin Project. This shouldn't be too difficult but the main point of this project is to learn pseudocoding.
+
+
+getHumanInput
+getComputerInput
+
+if(humanInput < ComputerInput){
+    Computer++;
+} else if(humanInput > ComputerInput){
+    Human++;
+}else{
+    output "it's a draw lmfao"
+}
+
+we have to find a random number to multiply to since 100 doesn't divide equally in 3's. Don't want one option getting a better probably than another, even if it's miniscule. Refer to MDN's "What went wrong?" for how to use the Math.Random in JavaScript.
--- script.js
+++ script.js
@@ -1,1 +1,51 @@
-console.log("Hello, world!");
+function getComputerChoice(){
+    let computerNumberChoice = Math.floor(Math.random() * 3);
+    let computerChoice = "";
+
+    if(computerNumberChoice === 2){
+        computerChoice = "scissors";
+    } else if(computerNumberChoice === 1){
+        computerChoice = "rock";
+    } else {
+        computerChoice = "paper";
+    }
+
+    return computerChoice;
+}
+
+function getHumanChoice(){
+    var validAnswer = false;
+    let humanChoice = "";
+
+    while(!validAnswer){
+        humanChoice = prompt("Rock, paper, or scissors?");
+
+        if(humanChoice === null){
+            console.log("Give an answer.");
+        }else if(humanChoice === ""){
+            console.log("Did you wanna type something or?");
+        } else if(humanChoice.toLowerCase() === "rock" || humanChoice.toLowerCase() === "paper" || humanChoice.toLowerCase() === "scissors"){
+            validAnswer = true;
+        } else {
+            console.log("Quit fatfingering the keyboard and type.");
+        }
+    }
+    console.log(humanChoice);
+    return humanChoice;
+}
+
+function playRound(){
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+
+}
+
+function playGame(){
+    for(let i = 0; i < 5; i++){
+        playRound();
+    }
+}
+
+
+
+playGame();
